09f51c6c84fa05c250d78779d6df8c8ba534a978
Anderson Mesquita <andersonvom@gmail.com>
2020-04-15 11:03:16 -0400

    Reformat code to match google-java-format

--- src/main/java/it/xpug/kata/birthday_greetings/Main.java
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.java
@@ -1,16 +1,15 @@
 package it.xpug.kata.birthday_greetings;
 
-import java.io.*;
+import java.io.IOException;
 import java.text.ParseException;
-
-import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 public class Main {
 
-	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
-		BirthdayService service = new BirthdayService();
-		service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
-	}
-
+  public static void main(String[] args)
+      throws AddressException, IOException, ParseException, MessagingException {
+    BirthdayService service = new BirthdayService();
+    service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
+  }
 }
--- src/test/java/it/xpug/kata/birthday_greetings/AcceptanceTest.java
+++ src/test/java/it/xpug/kata/birthday_greetings/AcceptanceTest.java
@@ -1,49 +1,52 @@
 package it.xpug.kata.birthday_greetings;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import com.dumbster.smtp.SimpleSmtpServer;
+import com.dumbster.smtp.SmtpMessage;
 import java.util.List;
-import org.junit.*;
-
-import com.dumbster.smtp.*;
-
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AcceptanceTest {
 
-	private static final int NONSTANDARD_PORT = 9999;
-	private BirthdayService birthdayService;
-	private SimpleSmtpServer mailServer;
+  private static final int NONSTANDARD_PORT = 9999;
+  private BirthdayService birthdayService;
+  private SimpleSmtpServer mailServer;
 
-	@Before
-	public void setUp() throws Exception {
-		mailServer = SimpleSmtpServer.start(NONSTANDARD_PORT);
-		birthdayService = new BirthdayService();
-	}
+  @Before
+  public void setUp() throws Exception {
+    mailServer = SimpleSmtpServer.start(NONSTANDARD_PORT);
+    birthdayService = new BirthdayService();
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		mailServer.stop();
-		Thread.sleep(200);
-	}
+  @After
+  public void tearDown() throws Exception {
+    mailServer.stop();
+    Thread.sleep(200);
+  }
 
-	@Test
-	public void willSendGreetings_whenItsSomebodysBirthday() throws Exception {
+  @Test
+  public void willSendGreetings_whenItsSomebodysBirthday() throws Exception {
 
-		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/10/08"), "localhost", NONSTANDARD_PORT);
+    birthdayService.sendGreetings(
+        "employee_data.txt", new XDate("2008/10/08"), "localhost", NONSTANDARD_PORT);
 
-		assertEquals("message not sent?", 1, mailServer.getReceivedEmails().size());
-		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmails().get(0);
-		assertEquals("Happy Birthday, dear John!", message.getBody());
-		assertEquals("Happy Birthday!", message.getHeaderValue("Subject"));
-		List<String> recipients = message.getHeaderValues("To");
-		assertEquals(1, recipients.size());
-		assertEquals("john.doe@foobar.com", recipients.get(0));
-	}
+    assertEquals("message not sent?", 1, mailServer.getReceivedEmails().size());
+    SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmails().get(0);
+    assertEquals("Happy Birthday, dear John!", message.getBody());
+    assertEquals("Happy Birthday!", message.getHeaderValue("Subject"));
+    List<String> recipients = message.getHeaderValues("To");
+    assertEquals(1, recipients.size());
+    assertEquals("john.doe@foobar.com", recipients.get(0));
+  }
 
-	@Test
-	public void willNotSendEmailsWhenNobodysBirthday() throws Exception {
-		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/01/01"), "localhost", NONSTANDARD_PORT);
+  @Test
+  public void willNotSendEmailsWhenNobodysBirthday() throws Exception {
+    birthdayService.sendGreetings(
+        "employee_data.txt", new XDate("2008/01/01"), "localhost", NONSTANDARD_PORT);
 
-		assertEquals("what? messages?", 0, mailServer.getReceivedEmails().size());
-	}
+    assertEquals("what? messages?", 0, mailServer.getReceivedEmails().size());
+  }
 }
