9ea91642fbe84b58e701b8ef20b814b9db1c62b8
Brittany Blassingill <brittanyb@spotify.com>
2020-05-13 17:16:23 -0500

    Enabled leap year greetings

--- src/main/java/it/xpug/kata/birthday_greetings/BirthDate.java
+++ src/main/java/it/xpug/kata/birthday_greetings/BirthDate.java
@@ -5,14 +5,20 @@ import java.util.Objects;
 
 public class BirthDate {
 
+  public static final int FEB_29 = 60;
+  public static final int FEB_28 = 59;
   private LocalDate date;
 
   BirthDate(LocalDate date) {
     this.date = date;
   }
 
-  public boolean isBirthday(LocalDate otherDate) {
-    return date.getDayOfYear() == otherDate.getDayOfYear();
+  public boolean isBirthday(LocalDate currentDate) {
+    if(date.getDayOfYear() == FEB_29 && !currentDate.isLeapYear()) {
+      return currentDate.getDayOfYear() == FEB_28;
+    }
+
+    return date.getDayOfYear() == currentDate.getDayOfYear();
   }
 
   @Override
--- src/test/java/it/xpug/kata/birthday_greetings/BirthDateTest.java
+++ src/test/java/it/xpug/kata/birthday_greetings/BirthDateTest.java
@@ -1,5 +1,7 @@
 package it.xpug.kata.birthday_greetings;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -18,5 +20,23 @@ class BirthDateTest {
     assertTrue("same", birthDate.isBirthday(sameDay));
     assertFalse("not same day", birthDate.isBirthday(notSameDay));
     assertFalse("not same month", birthDate.isBirthday(notSameMonth));
+  }
+
+  @Test
+  void isLeapBirthdate() {
+    BirthDate birthDate = new BirthDate(LocalDate.parse("2004-02-29"));
+    LocalDate feb29 = LocalDate.parse("2004-02-29");
+    LocalDate feb28 = LocalDate.parse("2004-02-28");
+
+    assertThat(birthDate.isBirthday(feb29), equalTo(true));
+    assertThat(birthDate.isBirthday(feb28), equalTo(false));
+  }
+
+  @Test
+  void isBirthday_notLeapYearFeb28() {
+    BirthDate birthDate = new BirthDate(LocalDate.parse("2004-02-29"));
+    LocalDate date = LocalDate.parse("2005-02-28");
+
+    assertThat(birthDate.isBirthday(date), equalTo(true));
   }
 }
